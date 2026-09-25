49f65cdcad68319c9fa99d984d09d00ca379d09c
Jocelyn P. Mahlatse <kmahlatse123@gmail.com>
2026-09-25 23:12:12 +0200

    Change Firebase config to Supabase client

--- src/pages/admin/AdminClasses.jsx
+++ src/pages/admin/AdminClasses.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { db } from '../../firebase/config'
-import { collection, getDocs, addDoc, deleteDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../../supabase/client'
 
 const s = { navy: '#001F3F', turquoise: '#40E0D0' }
 
