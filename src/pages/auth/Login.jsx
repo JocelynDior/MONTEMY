8a003228ca960a5538c49e2bab67dded109b2247
Jocelyn P. Mahlatse <kmahlatse123@gmail.com>
2026-09-25 23:06:43 +0200

    Switch authentication from Firebase to Supabase

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { auth, db } from '../../firebase/config'
-import { signInWithEmailAndPassword, onAuthStateChanged, setPersistence, browserLocalPersistence } from 'firebase/auth'
-import { doc, getDoc, collection, getDocs } from 'firebase/firestore'
+import { auth, db } from '../../supabase/client'
 import { BACKGROUND_VIDEO } from '../../config/media'
 import { glassCard, glassBtn, glassInput, addRipple, C } from '../../styles/glass'
 
